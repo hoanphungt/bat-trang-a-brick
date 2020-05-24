fd7efcfcfc24ad57041dd5b6ff8d88248a1f0b4f
Hoan Phung <hoanphung.ptso1@gmail.com>
2020-05-24 22:56:32 +0200

    More setups

---
+++ src/Components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from 'Logo.module.css';
+
+const Logo = () => {
+  return (
+    <div className={styles.Logo}>
+
+    </div>
+  )
+}
+
+export default Logo;
