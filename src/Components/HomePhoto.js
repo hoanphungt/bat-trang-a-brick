46d85bba4f1826976538fafda5bb9a49e62d73bf
Hoan Phung <hoanphung.ptso1@gmail.com>
2020-06-01 12:20:22 +0200

    Attempt to fix href target to intro div not working on safari

--- src/Components/HomePhoto.js
+++ src/Components/HomePhoto.js
@@ -9,7 +9,7 @@ const HomePhoto = () => {
       <div
         className={styles.HomePhotoScrolling}
       >
-        <a href='#introduction'>
+        <a href='/#introduction'>
           <img
             className={styles.ScrollingIcon}
             src='/images/scrolling-icon.gif'
