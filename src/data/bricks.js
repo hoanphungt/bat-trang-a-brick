ec741a8f71c94a5544b093e60e8e1356ecd6456f
Hoan Phung <hoanphung.ptso1@gmail.com>
2020-06-27 23:37:01 +0200

    Update photos of products

--- src/data/bricks.js
+++ src/data/bricks.js
@@ -7,7 +7,7 @@
 export const allBricks = {
   sample1: {
     id: 1,
-    name: 'sample 1',
+    name: 'gạch hoa văn',
     popular: true,
     urls: {
       // max 4 urls are allowed as explained in the top
@@ -29,10 +29,13 @@ export const allBricks = {
   },
   sample2: {
     id: 2,
-    name: 'sample 2',
+    name: 'gạch đặc Anh',
     popular: true,
     urls: {
       brickUrl: '/products/sample2/brick.jpg',
+      buildingUrl1: '/products/sample2/building-1.jpg',
+      buildingUrl2: '/products/sample2/building-2.jpg',
+      buildingUrl3: '/products/sample2/building-3.jpg',
     },
     brickIntro: `
       The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
@@ -47,10 +50,13 @@ export const allBricks = {
   },
   sample3: {
     id: 3,
-    name: 'sample 3',
+    name: 'gạch thẻ Anh',
     popular: true,
     urls: {
       brickUrl: '/products/sample3/brick.jpg',
+      buildingUrl1: '/products/sample3/building-1.jpg',
+      buildingUrl2: '/products/sample3/building-2.jpg',
+      buildingUrl3: '/products/sample3/building-3.jpg',
     },
     brickIntro: `
       The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
@@ -65,9 +71,13 @@ export const allBricks = {
   },
   sample4: {
     id: 4,
-    name: 'sample 4',
+    name: 'gạch sần Mỹ',
+    popular: true,
     urls: {
       brickUrl: '/products/sample4/brick.jpg',
+      buildingUrl1: '/products/sample4/building-1.jpg',
+      buildingUrl2: '/products/sample4/building-2.jpg',
+      buildingUrl3: '/products/sample4/building-3.jpg',
     },
     brickIntro: `
       The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
@@ -82,9 +92,11 @@ export const allBricks = {
   },
   sample5: {
     id: 5,
-    name: 'sample 5',
+    name: 'gạch bát giác',
+    popular: true,
     urls: {
       brickUrl: '/products/sample5/brick.jpg',
+      buildingUrl1: '/products/sample5/building-1.jpg',
     },
     brickIntro: `
       The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
@@ -99,9 +111,11 @@ export const allBricks = {
   },
   sample6: {
     id: 6,
-    name: 'sample 6',
+    name: 'gạch lục lăng',
+    popular: true,
     urls: {
       brickUrl: '/products/sample6/brick.jpg',
+      buildingUrl1: '/products/sample6/building-1.jpg',
     },
     brickIntro: `
       The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
@@ -116,9 +130,12 @@ export const allBricks = {
   },
   sample7: {
     id: 7,
-    name: 'sample 7',
+    name: 'gạch chữ thọ',
+    popular: true,
     urls: {
       brickUrl: '/products/sample7/brick.jpg',
+      buildingUrl1: '/products/sample7/building-1.jpg',
+      buildingUrl2: '/products/sample7/building-2.jpg',
     },
     brickIntro: `
       The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
@@ -133,10 +150,93 @@ export const allBricks = {
   },
   sample8: {
     id: 8,
-    name: 'sample 8',
+    name: 'gạch lát nền',
     popular: true,
     urls: {
       brickUrl: '/products/sample8/brick.jpg',
+      buildingUrl1: '/products/sample8/building-1.jpg',
+    },
+    brickIntro: `
+      The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
+      This Loess mainly consists of a silt-like fraction, suited ideally for the manufacturing of hand form bricks.
+      By using specific sand types for surface covering, the desired colour is achieved.
+    `,
+    specifications: {
+      code: 'BB 8328',
+      color: 'Orange brown with dark shades.',
+      format: 'WF ± 210 x 100 x 50 mm'
+    }
+  },
+  sample9: {
+    id: 9,
+    name: 'gạch vồ',
+    popular: true,
+    urls: {
+      brickUrl: '/products/sample9/brick.jpg',
+      buildingUrl1: '/products/sample9/building-1.jpg',
+      buildingUrl2: '/products/sample9/building-2.jpg',
+      buildingUrl3: '/products/sample9/building-3.jpg',
+    },
+    brickIntro: `
+      The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
+      This Loess mainly consists of a silt-like fraction, suited ideally for the manufacturing of hand form bricks.
+      By using specific sand types for surface covering, the desired colour is achieved.
+    `,
+    specifications: {
+      code: 'BB 8328',
+      color: 'Orange brown with dark shades.',
+      format: 'WF ± 210 x 100 x 50 mm'
+    }
+  },
+  sample10: {
+    id: 10,
+    name: 'gạch thất cổ',
+    popular: true,
+    urls: {
+      brickUrl: '/products/sample10/brick.jpg',
+      buildingUrl1: '/products/sample10/building-1.jpg',
+      buildingUrl2: '/products/sample10/building-2.jpg',
+      buildingUrl3: '/products/sample10/building-3.jpg',
+    },
+    brickIntro: `
+      The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
+      This Loess mainly consists of a silt-like fraction, suited ideally for the manufacturing of hand form bricks.
+      By using specific sand types for surface covering, the desired colour is achieved.
+    `,
+    specifications: {
+      code: 'BB 8328',
+      color: 'Orange brown with dark shades.',
+      format: 'WF ± 210 x 100 x 50 mm'
+    }
+  },
+  sample11: {
+    id: 11,
+    name: 'gạch hoa chanh',
+    popular: true,
+    urls: {
+      brickUrl: '/products/sample11/brick.jpg',
+      buildingUrl1: '/products/sample11/building-1.jpg',
+    },
+    brickIntro: `
+      The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
+      This Loess mainly consists of a silt-like fraction, suited ideally for the manufacturing of hand form bricks.
+      By using specific sand types for surface covering, the desired colour is achieved.
+    `,
+    specifications: {
+      code: 'BB 8328',
+      color: 'Orange brown with dark shades.',
+      format: 'WF ± 210 x 100 x 50 mm'
+    }
+  },
+  sample12: {
+    id: 12,
+    name: 'gạch mẫu',
+    popular: true,
+    urls: {
+      brickUrl: '/products/sample12/brick.jpg',
+      buildingUrl1: '/products/sample12/building-1.jpg',
+      buildingUrl2: '/products/sample12/building-2.jpg',
+      buildingUrl3: '/products/sample12/building-3.jpg',
     },
     brickIntro: `
       The raw materials are excavated in Weichsel loam layers, the local loam of Aeolian origin dating from the Ice Age.
