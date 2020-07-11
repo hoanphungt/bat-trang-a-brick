e988b19f8d25e3c3eb897c057e51d58d85b16ae6
Hoan Phung <hoanphung.ptso1@gmail.com>
2020-07-11 20:54:32 +0200

    Set Vietnamese as default language

--- src/index.js
+++ src/index.js
@@ -9,23 +9,11 @@ import { createIntl, createIntlCache, RawIntlProvider } from "react-intl";
 import english from './translations/en.json';
 import vietnamese from './translations/vi.json';
 
-// Get current user's locale
-const getCurrentLocale = () => {
-  if (navigator.languages) {
-    return navigator.languages[0];
-  } else {
-    return navigator.language;
-  };
-};
-const currentLocale = getCurrentLocale();
-
-// Set app's language by either current user's locale or their selected language preference
-// currently the app only supports English and Vietnamese with English as default language
+// Set app's language in Vietnamese by default or user's selected preference
+// currently the app only supports English and Vietnamese
 let preferredLocale = localStorage.getItem('preferred-language');
 if (!preferredLocale) {
-  preferredLocale = (
-    currentLocale === 'vi' ? 'vi' : 'en'
-  );
+  preferredLocale = 'vi'; // default language Vietnamese
 };
 
 const cache = createIntlCache();
